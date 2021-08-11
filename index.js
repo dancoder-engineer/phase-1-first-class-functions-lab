107f1380b8debc62f3d4b700cf497b00af88eb1e
Dan Seminara <japaneseforgaming@gmail.com>
2021-08-11 17:01:02 -0400

    commit tree 88e8658e6d94c7a386f471080c51a039e275849c

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solution in this file!
+
+const returnFirstTwoDrivers = function(array) {
+    let returned = [];
+    returned.push(array[0]);
+    returned.push(array[1]);
+    return returned;
+}
+
+const returnLastTwoDrivers = function(array) {
+    let returned = [];
+    returned.push(array[array.length-2]);
+    returned.push(array[array.length-1]);
+    return returned;
+}
+
+
+let selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+
+function createFareMultiplier(nu) {
+    return function(fare) { return fare * nu; };
+}
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers(array, funct) {
+    return funct(array);
+}
